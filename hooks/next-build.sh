09a725a355c7ee9f6046e228d291f1e2ea780fa5
chriswudev <chris.wu.tiff@gmail.com>
2021-06-16 07:59:53 -0400

    update build script

--- hooks/next-build.sh
+++ hooks/next-build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cd ../contracts/
+cd contracts/
 echo "✅ --- 0 ---"
 yarn
 echo "✅ --- 1 ---"
 yarn add hardhat
-yarn add typescript -D
 echo "✅ --- 2 ---"
 export TS_NODE_TRANSPILE_ONLY=1 && yarn hardhat compile
 echo "✅ --- 3 ---"
@@ -17,7 +16,7 @@ cd ../frontend/
 echo "✅ --- 6 ---"
 yarn
 echo "✅ --- 7 ---"
-yarn next build
+yarn build
 echo "--- 8 ---"
 # yarn next start
 echo "--- 9 ---"
